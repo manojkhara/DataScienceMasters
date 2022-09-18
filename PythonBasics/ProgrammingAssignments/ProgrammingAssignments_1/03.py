7fe01a6248139d849b4edfd49d921c58e7dd2532
manojkhara <58291240+manojkhara@users.noreply.github.com>
2022-09-18 21:08:57 +0530

    Add files via upload

---
+++ PythonBasics/ProgrammingAssignments/ProgrammingAssignments_1/03.py
@@ -0,0 +1,9 @@
+#3. Write a Python program to find the area of a triangle?
+
+# area = 1/2 * base * height
+
+base = int(input("enter base length of traingle"))
+height = int(input("enter height of triangle"))
+
+area = base * height / 2
+print(area)
---
+++ PythonBasics/ProgrammingAssignments/ProgrammingAssignments_1/04.py
@@ -0,0 +1,9 @@
+#4. Write a Python program to swap two variables?
+
+first_num = int(input("first num"))
+second_num = int(input("second num"))
+
+first_num,second_num = second_num,first_num
+
+print(first_num)
+print(second_num)
