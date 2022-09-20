d572cc50d39f910543a31c50796111f9c93dfe40
manojkhara <58291240+manojkhara@users.noreply.github.com>
2022-09-20 18:35:04 +0530

    Add files via upload

---
+++ PythonBasics/ProgrammingAssignments/ProgrammingAssignments_2/02.py
@@ -0,0 +1,8 @@
+#2. Write a Python program to do arithmetical operations addition and division.?
+a = int(input("enter a number"))
+b = int(input('enter another number'))
+
+print(a+b)    #addition
+print(a/2)    #division
+
+
---
+++ PythonBasics/ProgrammingAssignments/ProgrammingAssignments_2/05.py
@@ -0,0 +1,8 @@
+#5. Write a Python program to generate a random number?
+import random
+
+a = random.random()
+print(a)
+
+b = random.randint(0,15)
+print(b)
